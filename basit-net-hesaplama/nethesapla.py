cf54d20a205978f18f00791b1c8b2fdc0035e34d
selcuk <tatarselcuk27@gmail.com>
2020-06-29 11:43:45 +0300

    dogru yanlis opsiyonu getirildi

--- basit-net-hesaplama/nethesapla.py
+++ basit-net-hesaplama/nethesapla.py
@@ -1,8 +1,8 @@
 
 dogru = int(input("Dogru Sayinizi Giriniz: "))
 yanlis = int(input("Yanlis Sayinizi Giriniz: "))
-
-net = dogru - (yanlis / 4)
-
-print(f'Netiniz: {net} ')
+netdegeri = int(input("Kac Yanlis Dogrunu Goturuyor: "))
+net = dogru - (yanlis / netdegeri)
+sinirli_float_degeri = round(net, 2)
+print(f'Netiniz: {sinirli_float_degeri} ')
 
